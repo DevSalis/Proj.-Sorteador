f2de5f20495666f2a86ad99321390ed2f9a9935b
MrSalis <philip.cost@hotmail.com>
2023-12-07 17:20:19 -0300

    add-if-and-alert-altera-css

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function sortear() {
     
     const min = Math.ceil(document.querySelector(".minimo").value);
-    const max = Math.floor(document.querySelector(".maximo").value); 
-
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    const max = Math.floor(document.querySelector(".maximo").value);
     const paragrafoResult = document.querySelector(".resultado");
 
-    sortear (paragrafoResult.innerHTML = result)
+    if (min >= max) {
+        alert("O primeiro número não pode ser iqual ou maior que o segundo")
+      }
+
+    else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        
+        sortear (paragrafoResult.innerHTML = result)
+    } 
+
+    
 }
 
